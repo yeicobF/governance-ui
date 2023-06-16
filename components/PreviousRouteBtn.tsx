6b6791c8ed89aa6bf1716109a45bee98973acae7
yeicobF <jacobrdz34@gmail.com>
2023-06-16 03:30:22 -0600

    fix(ui): add optional children to previous route button

--- components/PreviousRouteBtn.tsx
+++ components/PreviousRouteBtn.tsx
@@ -3,14 +3,16 @@ import useRouterHistory from '@hooks/useRouterHistory'
 import Link from 'next/link'
 import React from 'react'
 
-const PreviousRouteBtn = ({children}) => {
+const PreviousRouteBtn = (props: {
+  children?: React.ReactNode
+}) => {
   const { getLastRoute } = useRouterHistory()
   const lastUrl = getLastRoute() as string
   return lastUrl ? (
     <Link href={lastUrl ? lastUrl : ''}>
       <a className="default-transition flex items-center text-fgd-2 text-sm transition-all hover:text-fgd-3">
         {
-          children ?? <>
+          props.children ?? <>
             <ChevronLeftIcon className="h-6 w-6 " />
             Back
           </>
